48e0e6a6819cd1329b2280ea86d250faace773e1
VVedhant <vvedhant312@gmail.com>
2023-10-20 11:17:58 +0000

    LayeredArchitecture

---
+++ DataLayer/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Entity;
+namespace DataLayer;
+public class DataAccess
+{
+    public static List<Movie> movies = new List<Movie>(){
+        new Movie {ID=1, Name="Toofan", RYear = 2021, Ratings =3},
+        new Movie {ID=2, Name="Ludo", RYear = 2020, Ratings =4},
+        new Movie {ID=3, Name="Gunjan", RYear = 2020, Ratings =1},
+        new Movie {ID=4, Name="Big Bull", RYear = 2021, Ratings =3},
+        new Movie {ID=5, Name="Laxmi", RYear = 2020, Ratings =2}
+    };
+    public List<Movie> GetMovies()
+    {
+        return movies;
+    }
+}
